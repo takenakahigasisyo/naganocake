937eb48065d29b11573286c54a97626695b63106
EC2 Default User <ec2-user@ip-172-31-5-32.ap-northeast-1.compute.internal>
2023-11-17 01:35:36 +0000

    o.commit

--- db/migrate/20231116113530_create_cart_items.rb
+++ db/migrate/20231116113530_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       
-      t.integer :amount
+      t.integer :amount, null: false
       
       t.timestamps
     end
