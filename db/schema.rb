d6bc083f54283c3526a295a5364b5eaba2a402bc
Mika Higashizima <144198758+higashizima@users.noreply.github.com>
2023-11-23 08:59:20 +0000

    マイグレーションファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_11_20_091339) do
-
+ActiveRecord::Schema.define(version: 2023_11_19_061938) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
