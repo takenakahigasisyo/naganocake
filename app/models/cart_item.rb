9d9b14115798e1731c278c5ff872b2c00a1f891f
EC2 Default User <ec2-user@ip-172-31-5-32.ap-northeast-1.compute.internal>
2023-11-24 08:43:19 +0000

    cart_item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -7,13 +7,13 @@ class CartItem < ApplicationRecord
     amount * item.tax_calc
   end
 
-  def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/test_cow.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image
-  end
+  # def get_image(width, height)
+  #   unless image.attached?
+  #     file_path = Rails.root.join('app/assets/images/test_cow.jpg')
+  #     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   image
+  # end
   
 
 	def total_price
