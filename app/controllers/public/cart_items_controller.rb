29b70a91597eeb5e8637a180f097e87940e7f0bd
EC2 Default User <ec2-user@ip-172-31-5-32.ap-northeast-1.compute.internal>
2023-11-25 05:52:55 +0000

    確認

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,24 +12,32 @@ class Public::CartItemsController < ApplicationController
     redirect_back(fallback_location: items_path)
   end
 
-  def destroy_all
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy_all
-    redirect_to '/items'
-  end
 
-  
+
+
   # みかん記載。item.showページから商品をカート追加するために必要
   def create
     cart_item = CartItem.new(cart_item_params)
 		if cart_item.save
-			redirect_to cart_items_path 
+			redirect_to cart_items_path
 		else
 			flash[:notice] = "個数を選択してください"
 			redirect_to request.referer
 		end
   end
-  
+
+  def destroy
+    cart_item = CartItem.find(cart_item_params)
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    cart_item = CartItem.all
+    cart_item.destroy_all
+    redirect_to items_path
+  end
+
   private
   def cart_item_params
    params.require(:cart_item).permit(:item_id, :amount, :customer_id)# customer_id追記（みかん）
